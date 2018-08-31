b87d2b3d108b9345d1f596b014bf3e589cb1be95
Cagatay Tanyildiz <cagatay@cagataytanyildiz.com>
2018-09-01 00:31:32 +0300

    Edit for converting strings to TCL command

--- listopenfiles.py
+++ listopenfiles.py
@@ -6,9 +6,9 @@ class ListOpenFilesCommand(sublime_plugin.TextCommand):
         window = sublime.active_window()
         views = window.views()
 
-        fileNames = ''
+        fileNames = 'set list {'
         for view in views:
             if view and view.file_name():
-                fileNames += os.path.abspath(view.file_name())+'\n'
+                fileNames += '{'+os.path.abspath(view.file_name())+'}\n'
 
-        window.new_file().insert(edit, 0, "List of open files:\n\n"+fileNames)
+        window.new_file().insert(edit, 0, "List of open files:\n\n"+fileNames+'}')
